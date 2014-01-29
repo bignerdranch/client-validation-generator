53da9a209519a868d7375a629aaabcbf098ca66a
Josh Justice <josh@need-bee.com>
2014-01-29 14:15:38 -0500

    added comment about where testing method came from

--- tests/NpmWeb/Test/TestCase.php
+++ tests/NpmWeb/Test/TestCase.php
@@ -4,6 +4,9 @@ namespace NpmWeb\Test;
 
 class TestCase extends \PHPUnit_Framework_TestCase {
 
+	/**
+	 * @see http://stackoverflow.com/questions/249664/best-practices-to-test-protected-methods-with-phpunit
+	 */
 	protected static function getMethod($className, $methodName) {
 		$class = new \ReflectionClass($className);
 		$method = $class->getMethod($methodName);
