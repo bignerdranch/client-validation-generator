5f6f72d3474b24cea9cc81b37c6bc90627777298
Josh Justice <josh@need-bee.com>
2014-01-31 14:16:47 -0500

    commented abstract class

--- src/NpmWeb/ClientValidationGenerator/BaseJqueryValidationGenerator.php
+++ src/NpmWeb/ClientValidationGenerator/BaseJqueryValidationGenerator.php
@@ -14,11 +14,29 @@ abstract class BaseJqueryValidationGenerator
 	implements \NpmWeb\ClientValidationGenerator\ClientValidationGeneratorInterface
 {
 
+	/**
+	 * Generates a script tag with the jQuery Validator binding.
+	 * Delegates to the abstract generateClientValidatorRules() to
+	 * translate from server-side rules to jQuery rules.
+	 */
 	public function generateClientValidatorCode( $allRules, $formId ) {
 		$mappedRules = $this->generateClientValidatorRules( $allRules );
-		return '<script type="text/javascript">$(function(){ $("#'. $formId.'").validate({ rules: '.json_encode($mappedRules).'}); })</script>';
+		return '<script type="text/javascript">'
+			+ '$(function(){'
+			+'$("#'. $formId.'").validate({ rules: '.json_encode($mappedRules).'});'
+			+ '});'
+			+ '</script>';
 	}
 
+	/**
+	 * Takes source rules in some format and translates them into jQuery
+	 * Validator rules.
+	 *
+	 * @param $sourceRules rules in the format a server-side validator uses
+	 * @return a PHP array structure of rules in the format jQuery uses, i.e.
+	 *
+	 * array( 'myfield' => array( 'required' => true, 'maxlength' => 20 ) )
+	 */
 	protected abstract function generateClientValidatorRules( $sourceRules );
 
 }
