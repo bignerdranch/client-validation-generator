0c20663b17c7f2bb6e9e7ed9edaf0adc55bcf328
Josh Justice <josh@need-bee.com>
2015-01-28 15:07:52 -0500

    updates spacing

--- src/NpmWeb/ClientValidationGenerator/Laravel/JqueryValidationGenerator.php
+++ src/NpmWeb/ClientValidationGenerator/Laravel/JqueryValidationGenerator.php
@@ -6,64 +6,64 @@ namespace NpmWeb\ClientValidationGenerator\Laravel;
  * Generates jQuery validation from Laravel server-side rules.
  */
 class JqueryValidationGenerator
-	extends \NpmWeb\ClientValidationGenerator\BaseJqueryValidationGenerator
+    extends \NpmWeb\ClientValidationGenerator\BaseJqueryValidationGenerator
 {
 
-	protected $ruleMappings;
+    protected $ruleMappings;
 
-	public function __construct(
-		$ruleMappings,
-		$useRequireJs,
-		$packageName = array('jquery-validation'),
-		$functionName = 'validate'
-	) {
-		parent::__construct($useRequireJs,$packageName,$functionName);
-		$this->ruleMappings = $ruleMappings;
-	}
+    public function __construct(
+        $ruleMappings,
+        $useRequireJs,
+        $packageName = array('jquery-validation'),
+        $functionName = 'validate'
+    ) {
+        parent::__construct($useRequireJs,$packageName,$functionName);
+        $this->ruleMappings = $ruleMappings;
+    }
 
-	protected function generateClientValidatorRules( $allRules ) {
-		$jQueryValidatorRuleMappings = $this->ruleMappings;
+    protected function generateClientValidatorRules( $allRules ) {
+        $jQueryValidatorRuleMappings = $this->ruleMappings;
 
-		$mappedRules = array();
-		foreach( $allRules as $field => $fieldRules ) {
+        $mappedRules = array();
+        foreach( $allRules as $field => $fieldRules ) {
 
-			$mappedRules[$field] = array();
+            $mappedRules[$field] = array();
 
-			if( !is_array($fieldRules) ) {
-				$fieldRules = explode('|',$fieldRules);
-			}
-			foreach( $fieldRules as $rule ) {
-				// extract rule data
-				if( FALSE === strpos( $rule, ':' ) ) {
-					$ruleName = $rule;
-					$param = null;
-				} else {
-					$parts = explode(':', $rule);
-					$ruleName = $parts[0];
-					$param = $parts[1];
-				}
+            if( !is_array($fieldRules) ) {
+                $fieldRules = explode('|',$fieldRules);
+            }
+            foreach( $fieldRules as $rule ) {
+                // extract rule data
+                if( FALSE === strpos( $rule, ':' ) ) {
+                    $ruleName = $rule;
+                    $param = null;
+                } else {
+                    $parts = explode(':', $rule);
+                    $ruleName = $parts[0];
+                    $param = $parts[1];
+                }
 
-				// find out if mapped
-				if( !array_key_exists( $ruleName, $jQueryValidatorRuleMappings ) ) {
-					continue;
-				}
+                // find out if mapped
+                if( !array_key_exists( $ruleName, $jQueryValidatorRuleMappings ) ) {
+                    continue;
+                }
 
-				// if mapped, get new data
-				$ruleMapping = $jQueryValidatorRuleMappings[$ruleName];
-				if( !is_array($ruleMapping) ) {
-					$mappedRuleName = $ruleMapping;
-					$mappedParam = $param;
-				} else { // is array
-					$mappedRuleName = $ruleMapping['rule'];
-					$mappedParam = $ruleMapping['param']($param);
-				}
-				if( null == $mappedParam ) {
-					$mappedParam = true;
-				}
+                // if mapped, get new data
+                $ruleMapping = $jQueryValidatorRuleMappings[$ruleName];
+                if( !is_array($ruleMapping) ) {
+                    $mappedRuleName = $ruleMapping;
+                    $mappedParam = $param;
+                } else { // is array
+                    $mappedRuleName = $ruleMapping['rule'];
+                    $mappedParam = $ruleMapping['param']($param);
+                }
+                if( null == $mappedParam ) {
+                    $mappedParam = true;
+                }
 
-				// add to jQuery array
-				$mappedRules[$field][$mappedRuleName] = $mappedParam;
-			}
+                // add to jQuery array
+                $mappedRules[$field][$mappedRuleName] = $mappedParam;
+            }
             if (array_key_exists('minlength', $mappedRules[$field])) {
                 if (array_key_exists('number', $mappedRules[$field])) {
                     $mappedRules[$field]['min'] = $mappedRules[$field]['minlength'];
@@ -76,9 +76,9 @@ class JqueryValidationGenerator
                     unset($mappedRules[$field]['maxlength']);
                 }
             }
-		}
+        }
 
-		return $mappedRules;
-	}
+        return $mappedRules;
+    }
 
 }
