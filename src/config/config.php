317f9328d52eeeb4aa377d5e5b62db0507531f04
Josh Justice <josh@need-bee.com>
2015-01-28 15:08:55 -0500

    updates spacing

--- src/config/config.php
+++ src/config/config.php
@@ -1,27 +1,27 @@
 <?php
 
 return array(
-	'driver' => 'jquery',
+    'driver' => 'jquery',
 
-	'useRequireJs' => true,
-	'packageName' => ['jquery-validation'],
-	'functionName' => 'validate',
+    'useRequireJs' => true,
+    'packageName' => ['jquery-validation'],
+    'functionName' => 'validate',
 
-	'ruleMappings' => array(
-		'required' => 'required',
-		'min' => 'minlength',
-		'max' => 'maxlength',
-		'date' => 'date',
-		'email' => 'email',
-		'url' => 'url',
-		'numeric' => 'number',
-		'before_now' => 'before_now',
-		'same' => array(
-			'rule' => 'equalTo',
-			'param' => function( $param ) {
-				return 'input[name=' . $param . ']';
-			},
-		),
-	),
+    'ruleMappings' => array(
+        'required' => 'required',
+        'min' => 'minlength',
+        'max' => 'maxlength',
+        'date' => 'date',
+        'email' => 'email',
+        'url' => 'url',
+        'numeric' => 'number',
+        'before_now' => 'before_now',
+        'same' => array(
+            'rule' => 'equalTo',
+            'param' => function( $param ) {
+                return 'input[name=' . $param . ']';
+            },
+        ),
+    ),
 
 );
